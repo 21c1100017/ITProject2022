0de3767c22856726b7e1e207c5dfb3c4e19f91d1
吉田 健人 <21c1100017yk@edu.tech.ac.jp>
2022-05-25 17:51:47 +0900

    masterブランチ内ファイル全消去

--- create_user.sql
+++
@@ -1,2 +0,0 @@
-CREATE USER 'st'@'localhost' IDENTIFIED BY '1234';
-GRANT ALL ON `sns`.* TO 'st'@'localhost';
--- follow.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `follow` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `follow_id` int(11) NOT NULL,
-  `follower_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-)
