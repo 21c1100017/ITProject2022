839adf15e3d46f6fc789297b622e783b17e04bea
hirok910 <104411282+hirok910@users.noreply.github.com>
2022-05-26 16:02:57 +0900

    Add files via upload

---
+++ post.b/login/auto_login.php
@@ -0,0 +1,44 @@
+<?php 
+require('../db/dbconnect.php');
+
+//トークン発行
+function setLoginToken ($user_id) {
+    global $db;
+    if (isset($_COOKIE['token'])) {
+        $st = $db->prepare('DELETE FROM user_token WHERE token=?');
+        $st->execute(array($_COOKIE['token']));
+    }
+
+    $token = '';
+    $st = $db->prepare('SELECT * FROM user_token WHERE token=?');
+    for ($i = 0; $i < 100; $i++) {
+        $token_temp = bin2hex(openssl_random_pseudo_bytes(16));
+        $st->execute(array($token_temp));
+        if (!$st->fetch()) {
+            $token = $token_temp;
+            break;
+        }
+    }
+    if ($token == '') {
+        throw new Exception('token error');
+    }
+    //テーブルトークン保存
+    $st = $db->prepare('INSERT INTO user_token (token, id, insert_date) VALUES(?, ?, ?)');
+    $st-> execute(array($token, $user_id, date('Y-m-d H:i:s')));
+    //クッキーへトークンを保存
+    setcookie('token', $token, time() + 60 * 60 * 24 * 7, '/');
+}
+
+//自動ログイン
+function auto_login() {
+    global $db;
+    $st = $db->prepare('SELECT * FROM user_token WHERE token=?');
+    $st->execute(array($_COOKIE['token']));
+    if ($pass = $st->fetch()) {
+        session_regenerate_id(true);
+        $_SESSION['id'] = $pass['id'];
+        $_SESSION['time'] = time();
+        header('Location: ../acount/user_page.php');
+        exit();
+    }
+}
---
+++ post.b/login/login.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+require('../db/dbconnect.php');
+require('./auto_login.php');
+
+if (!empty($_COOKIE['token'])) {
+    auto_login();
+}
+
+//配列　定義
+$keywords = [
+    'post_email' => '',
+    'post_password' => '',
+    'err_failed' => ''
+];
+
+//エラー発生フラグ
+$error = false;
+
+//var_dump($_POST);
+//exit;
+
+if (!empty($_POST)) {
+    //ログインの処理
+    if($_POST['email'] != '' && $_POST['password'] != '') {
+        $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
+        $login->execute(array(
+            $_POST['email'],
+            sha1($_POST['password'])
+        ));
+        $member = $login->fetch();
+            //ログイン成功時
+            if($member) {
+                $_SESSION['id'] = $member['id'];
+                $_SESSION['time'] = time();
+
+                    //ログイントークンの生成
+                    if ($_POST['save'] == 'on') {
+                        setLoginToken($member['id']);
+                    }
+
+                header('Location: ../acount/user_page.php');
+                exit();
+            } else {
+                $error['login'] = 'failed' ;
+            }
+    } else {
+        $error['login'] = 'blank' ;
+    }
+}
+
+//エラー処理
+if (!empty($error)){
+    $keywords['post_email'] = $_POST['email'] ;
+    $keywords['post_password'] = $_POST['password'] ;
+
+    if ($error['login'] == 'failed') {
+        $keywords['err_failed'] = 'ログインに失敗しました。正しくご記入ください。';
+    }
+    if ($error['login'] == 'blank') {
+        $keywords['err_failed'] = 'メールアドレスとパスワードをご記入ください';
+    }
+} 
+
+//html接続
+$html = file_get_contents('./login.html');
+
+foreach($keywords as $key => $value) {
+    $html = str_replace('{{' . $key . '}}', htmlspecialchars($value, ENT_QUOTES), $html);
+}
+
+print($html);
+
+
+
+
