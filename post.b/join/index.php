16dcfe58c197d871fdf07120a9edf7bfde5cc8b1
吉田 健人 <84760512+21c1100017@users.noreply.github.com>
2022-05-30 18:08:31 +0900

    添削追加

--- post.b/join/index.php
+++ post.b/join/index.php
@@ -69,7 +69,7 @@ if (!empty($_POST)) {
     }
 
     if (!$error) {
-        $member = $db->prepare ('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
+        $member = $db->prepare ('SELECT COUNT(*) AS cnt FROM members WHERE email=?'); //SELECT FROM で良さそう
         $member -> execute(array($_POST['email']));
         $record = $member->fetch();
         if($record['cnt'] > 0) {
