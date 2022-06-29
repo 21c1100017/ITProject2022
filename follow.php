0ec39102ef2387bc72c780b464d8edb2403c734f
吉田 健人 <21c1100017yk@edu.tech.ac.jp>
2022-06-30 01:28:22 +0900

    ファイル名変更

--- follow.php
+++ follow.php
@@ -4,27 +4,40 @@ require_once("./database.php");
 
 session_start();
 
-if(!isset($_SESSION["follow"])){
-    $_SESSION["follow"] = false;
-}
+$follow_id = 1;
+$follower_id = 2;
 
-if(isset($_POST["follow"])){
-    if($_SESSION["follow"]){
-        $_SESSION["follow"] = false;
-    }else{
-        $_SESSION["follow"] = true;
-    }
-}
+//follow_idとfollwer_idのカラムをfollowテーブルから取ってくる
+$sql = "SELECT * FROM `follow` WHERE `follow_id` = ? and `follower_id` = ?;";
+$stmt = $mysql->prepare($sql);
+$stmt->execute(array($follow_id, $follower_id));
+$res = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if($_SESSION["follow"]){
-    $msg = "フォロー解除";
-}else{
+if($res == false){
     $msg = "フォローする";
+}else{
+    $msg = "フォロー解除";
+}
+
+if(isset($_POST['follow'])){
+
+    if($res == false){
+        //データがない場合
+        $sql = 'INSERT INTO `follow` (`follow_id`, `follower_id`) VALUES (?, ?)';
+        $msg = "フォロー解除";
+    }else{
+        //データがある場合
+        $sql = 'DELETE FROM `follow` WHERE `follow_id` = ? AND `follower_id` = ?';
+        $msg = "フォローする";
+    }
+
+    $stmt = $mysql->prepare($sql);
+    $stmt->execute([$follow_id, $follower_id]);
+
 }
 
 $html = file_get_contents("./follow.html");
 $html = str_replace("{{msg}}", $msg, $html);
 
 print($html);
-var_dump($_SESSION);
 exit;
