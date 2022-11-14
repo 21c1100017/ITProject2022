bdf52ad4184e44fb35ccdd0f78f4302152423777
吉田 健人 <21c1100017yk@edu.tech.ac.jp>
2022-11-15 02:10:57 +0900

    update

--- functions/common/database.php
+++ functions/common/database.php
@@ -21,7 +21,7 @@ class database {
     private $bind_array = [];
 
     public function connect(){
-        $config = require('../config/config.php');
+        $config = include('../config/config.php');
         try{
             $this->db_handler = new PDO(
                 "mysql:host=".$config["DB_HOST"].";dbname=".$config["DB_DATABASE"],
---
+++ functions/timeline/timeline.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 
+ * 投稿を取得します。
+ * 
+ * @param int 取得する数を指定します。
+ * 
+ * @return string 結果を文字列で返します。
+ * 
+ */
+function get_posts(int $amount) : string {
+
+    
+}
