d60a8f4dee99a7857e39899705ec96de6b48d4d2
吉田 健人 <84760512+21c1100017@users.noreply.github.com>
2022-12-17 18:58:21 +0900

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 return [
     'TITLE' => 'WIP',
-    'DB_HOST' => 'localhost',
+    'DB_HOST' => 'melonkun.jp',
     'DB_NAME' => 'wip',
     'DB_USER' => 'wip_admin',
     'DB_PASSWORD' => 'wip_admin',
-    'DEBUG_MODE' => true,
+    'DEBUG_MODE' => false,
     'MAINTENANCE_MODE' => false
 ];
